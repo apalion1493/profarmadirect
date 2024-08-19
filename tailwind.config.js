ad2eed1bc6bf3a827a76dfedf2f832a533bdc3d3
Alexandr Petrovich <apalion97@gmail.com>
2024-08-19 10:06:59 +0300

    Add new HTML components and update .gitignore
    Introduce new Link, Pagination, and Textarea components with necessary HTML structure and styles. Also, add a new .gitignore file to exclude node_modules, specific CSS files, lock files, DS_Store, and .idea directories. Added the minified Splide.js library for carousel functionality.

---
+++ tailwind.config.js
@@ -0,0 +1,92 @@
+const options = require('./config') //options from config.js
+
+const allPlugins = {
+    typography: require('@tailwindcss/typography'),
+    forms: require('@tailwindcss/forms'),
+    containerQueries: require('@tailwindcss/container-queries'),
+}
+
+const plugins = Object.keys(allPlugins)
+    .filter(k => options.plugins[k])
+    .map(k => {
+        if (k in options.plugins && options.plugins[k]) {
+            return allPlugins[k]
+        }
+    })
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,php}'],
+    darkMode: 'class',
+    theme: {
+        backgroundSize: {
+            auto: 'auto',
+            cover: 'cover',
+            contain: 'contain',
+            '100%': '100%',
+            '100-auto': '100% auto',
+        },
+        fontFamily: {
+            sans: ['Sora', 'sans-serif'],
+        },
+        fontSize: {
+            none: ['0px', '0px'],
+            base: ['16px', '140%'],
+
+        },
+        screens: {
+            xl: {max: '1345px'},
+            lg: {max: '1023px'},
+            md: {max: '767px'},
+            sm: {max: '639px'},
+            sm2: {max: '480px'},
+
+            minsm: {min: '640px'},
+            minmd: {min: '768px'},
+            minlg: {min: '1024px'},
+            minxl: {min: '1346px'},
+        },
+        container: {
+            center: true,
+            padding: {
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
+        extend: {
+            boxShadow: {
+                'color-1': '0px 0px 25px 0px rgba(0, 0, 0, 0.15);'
+            },
+            colors: {
+                transparent: 'transparent',
+                primary: '#2E3436',
+                secondary: 'rgba(25, 24, 27, 0.05)',
+            },
+            letterSpacing: {
+
+            },
+            padding: {
+                '1/4': '25%',
+                '3/4': '75%',
+                '1/5': '20%',
+                '2/5': '40%',
+                '3/5': '60%',
+                '4/5': '80%',
+                '1/6': '16.666667%',
+                '5/6': '83.333333%',
+                '1/24': '4.166667%',
+                '2/24': '8.333333%',
+                '3/24': '12.5%',
+                '4/24': '16.666667%',
+                '5/24': '20.833333%',
+                '6/24': '25%',
+                '7/24': '29.166667%',
+                '8/24': '33.333333%',
+                '2.5': '10px',
+            },
+        },
+    },
+    plugins: plugins,
+}
